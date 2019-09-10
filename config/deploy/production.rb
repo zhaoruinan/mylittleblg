f2075917337b7695f88f4b9c759594885166d293
zhaoruinan <zhaoruinan2010@gmail.com>
2019-09-11 02:23:55 +0900

    ssh change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "138.128.200.100:28443", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "138.128.200.100", user: "deploy", roles: %w{app db web}, my_property: :my_value
 
 
 
